7051a3362ede8dd95e3e1161ea8caff6d3140d37
Ajanthan.HR <ajanthhr@in.ibm.com>
2023-01-21 12:59:34 +0530

    fix: added external configuration

--- src/main/java/com/store/catalogservice/CatalogServiceApplication.java
+++ src/main/java/com/store/catalogservice/CatalogServiceApplication.java
@@ -2,8 +2,10 @@ package com.store.catalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class CatalogServiceApplication {
     // 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
     public static void main(String[] args) {
--- src/main/java/com/store/catalogservice/controller/HomeController.java
+++ src/main/java/com/store/catalogservice/controller/HomeController.java
@@ -1,14 +1,21 @@
 package com.store.catalogservice.controller;
 
+import com.store.catalogservice.config.PolarProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController // Identifies a class defining handlers for REST/HTTP endpoints
 public class HomeController {
 
+    private final PolarProperties polarProperties;
+
+    public HomeController(PolarProperties polarProperties) {
+        this.polarProperties = polarProperties;
+    }
+
     @GetMapping("/") //  Handles GET requests to the root endpoint
     public String getGreeting() {
-        return "Welcome to the book catalog!";
+        return polarProperties.getGreeting();
     }
 
 }
